e86a3462477f5ae0b14449961ffd57008bccfa9f
Alfredo <oxarbitrage@gmail.com>
2019-03-06 11:10:34 -0300

    remove some duplicate code by using a loop function

--- bitshares_market_volume.lua
+++ bitshares_market_volume.lua
@@ -1,3 +1,5 @@
+
+-- Utility function to print a recursive table keys and values
 function DeepPrint (e)
     -- if e is a table, we should iterate over its elements
     if type(e) == "table" then
@@ -7,6 +9,42 @@ function DeepPrint (e)
         end
     else -- if not, we can just print it
         print(e)
+    end
+end
+
+-- loop throw results in initial query and in scroll, save data to markets table
+function loop_fills(hits)
+
+    for k, v in pairs(hits) do
+
+        total = total - 1
+        --print("first: " .. total)
+
+        new = true;
+
+        if k > 1 then
+
+            for k2, v2 in pairs(markets) do
+                if v2.quote == v._source.operation_history.op_object.pays.asset_id and
+                        v2.base == v._source.operation_history.op_object.receives.asset_id then
+
+                    markets[k2].quote_amount = markets[k2].quote_amount + v._source.operation_history.op_object.pays.amount
+                    markets[k2].base_amount = markets[k2].base_amount + v._source.operation_history.op_object.receives.amount
+                    new = false
+
+                    break
+                end
+            end
+        end
+
+        if new then
+            markets[k] = {
+                quote = v._source.operation_history.op_object.pays.asset_id,
+                base = v._source.operation_history.op_object.receives.asset_id,
+                quote_amount = v._source.operation_history.op_object.pays.amount,
+                base_amount = v._source.operation_history.op_object.receives.amount
+            }
+        end
     end
 end
 
@@ -76,92 +114,31 @@ json_body, error = response.json()
 markets = {}
 
 total = json_body.hits.total
+scroll_id = json_body._scroll_id
 -- print(total)
 
-for k, v in pairs(json_body.hits.hits) do
-
-    total = total - 1
-    --print("first: " .. total)
-
-    new = true;
-    scroll_id = json_body._scroll_id
-
-
-    if k > 1 then
-
-        for k2, v2 in pairs(markets) do
-            if v2.quote == v._source.operation_history.op_object.pays.asset_id and
-                    v2.base == v._source.operation_history.op_object.receives.asset_id then
-
-                markets[k2].quote_amount = markets[k2].quote_amount + v._source.operation_history.op_object.pays.amount
-                markets[k2].base_amount = markets[k2].base_amount + v._source.operation_history.op_object.receives.amount
-                new = false
-
-                break
-            end
-        end
-    end
-
-    if new then
-        markets[k] = {
-            quote = v._source.operation_history.op_object.pays.asset_id,
-            base = v._source.operation_history.op_object.receives.asset_id,
-            quote_amount = v._source.operation_history.op_object.pays.amount,
-            base_amount = v._source.operation_history.op_object.receives.amount
-        }
-	end
-end
+loop_fills(json_body.hits.hits)
 
 while total > 0 do
 
     scroll_query = [[
-        {
-            "scroll" : "1m",
-            "scroll_id" : "]] .. scroll_id .. [["
-        }
+{
+  "scroll" : "1m",
+  "scroll_id" : "]] .. scroll_id .. [["
+}
 
     ]]
     response = requests.get{'https://elasticsearch.bitshares-kibana.info/_search/scroll', data=scroll_query, headers = headers}
 
     json_body, error = response.json()
 
-    -- duplicated code
-    for k, v in pairs(json_body.hits.hits) do
+    loop_fills(json_body.hits.hits)
 
-        total = total - 1
-        -- print("scroll: " .. total)
-
-        new = true;
-        scroll_id = json_body._scroll_id
-
-        if k > 1 then
-
-            for k2, v2 in pairs(markets) do
-                if v2.quote == v._source.operation_history.op_object.pays.asset_id and
-                        v2.base == v._source.operation_history.op_object.receives.asset_id then
-
-                    markets[k2].quote_amount = markets[k2].quote_amount + v._source.operation_history.op_object.pays.amount
-                    markets[k2].base_amount = markets[k2].base_amount + v._source.operation_history.op_object.receives.amount
-                    new = false
-
-                    break
-                end
-            end
-        end
-
-        if new then
-            markets[k] = {
-                quote = v._source.operation_history.op_object.pays.asset_id,
-                base = v._source.operation_history.op_object.receives.asset_id,
-                quote_amount = v._source.operation_history.op_object.pays.amount,
-                base_amount = v._source.operation_history.op_object.receives.amount
-            }
-        end
-    end
 end
 
-
 -- DeepPrint(markets)
+
+-- need to make this more effcient as i am querying each record to just get an asset symbol
 for k, v in pairs(markets) do
 
     quote_query = [[
