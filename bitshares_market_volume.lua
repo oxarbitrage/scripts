f7df078e4e6421fe9b70930625b20e0a82fca5d7
Alfredo <oxarbitrage@gmail.com>
2019-03-06 19:53:44 -0300

    add some comments to code

--- bitshares_market_volume.lua
+++ bitshares_market_volume.lua
@@ -6,18 +6,19 @@ local config = require('config')
 
 headers = {['Content-Type'] = 'application/json'}
 
-response = requests.get{config.es_direct_connection .. '_search?scroll=1m', data=queries.first_query(config.from, config.to), headers = headers}
+markets = {} -- our table to store quote_id, base_id, quote_amount and base_amount
 
+-- queries ES with all fill orders taker side in period of time
+response = requests.get{config.es_direct_connection .. '_search?scroll=1m', data=queries.first_query(config.from, config.to), headers = headers}
 json_body, error = response.json()
 
-markets = {}
+total = json_body.hits.total -- total fill orders in period
+scroll_id = json_body._scroll_id -- We use scroll to query ES as it cant retrive by default more than 10000 results
 
-total = json_body.hits.total
-scroll_id = json_body._scroll_id
--- print(total)
-
+-- parse first 1000 results from first query
 functions.loop_fills(json_body.hits.hits)
 
+-- use scroll api to get the rest of the hits until total == 0
 while total > 0 do
 
     scroll_query = [[
@@ -28,42 +29,32 @@ while total > 0 do
 
     ]]
     response = requests.get{config.es_direct_connection .. '_search/scroll', data=scroll_query, headers = headers}
-
     json_body, error = response.json()
 
+    -- load markets with data
     functions.loop_fills(json_body.hits.hits)
-
 end
 
--- DeepPrint(markets)
-
--- need to make this more effcient as i am querying each record to just get an asset symbol
+-- loop collected data in markets table
 for k, v in pairs(markets) do
 
+    -- get quote name
     quote_query=queries.asset_query(v.quote)
     response = requests.get{config.es_direct_connection .. '_search', data=quote_query, headers = headers}
-
     json_body, error = response.json()
-
     quote_name = json_body.hits.hits[1]._source.symbol
-    --quote_name = ""
 
+    -- get base name
     base_query=queries.asset_query(v.base)
     response = requests.get{config.es_direct_connection .. '_search', data=base_query, headers = headers}
-
     json_body, error = response.json()
-
-    -- DeepPrint(json_body.hits.hits[2])
-
     base_name = json_body.hits.hits[1]._source.symbol
 
-
+    -- get ticker data
     response = requests.get{config.rest_bitshares_api .. 'get_ticker?base=BTS&quote=' .. quote_name .. '', headers = headers}
     json_body, error = response.json()
 
-    print(quote_name .. "/" .. base_name .. "," .. v.quote_amount/v.base_amount .. "," .. json_body.latest .. "," .. (v.quote_amount/v.base_amount)*json_body.latest)
-
-    -- print(base_name)
-    -- print(quote_name)
-
+    -- print csv
+    print(quote_name .. "/" .. base_name .. "," .. v.quote_amount/v.base_amount .. "," .. json_body.latest ..
+            "," .. (v.quote_amount/v.base_amount)*json_body.latest)
 end
