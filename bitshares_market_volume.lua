5489141d7766acd0c94c5fec88e5eb6f1406f6e5
Alfredo <oxarbitrage@gmail.com>
2019-03-06 17:36:11 -0300

    move functions to file

--- bitshares_market_volume.lua
+++ bitshares_market_volume.lua
@@ -1,57 +1,7 @@
-
--- Utility function to print a recursive table keys and values
-function DeepPrint (e)
-    -- if e is a table, we should iterate over its elements
-    if type(e) == "table" then
-        for k,v in pairs(e) do -- for every element in the table
-            print(k)
-            DeepPrint(v)       -- recursively repeat the same procedure
-        end
-    else -- if not, we can just print it
-        print(e)
-    end
-end
-
--- loop throw results in initial query and in scroll, save data to markets table
-function loop_fills(hits)
-
-    for k, v in pairs(hits) do
-
-        total = total - 1
-        --print("first: " .. total)
-
-        new = true;
-
-        if k > 1 then
-
-            for k2, v2 in pairs(markets) do
-                if v2.quote == v._source.operation_history.op_object.pays.asset_id and
-                        v2.base == v._source.operation_history.op_object.receives.asset_id then
-
-                    markets[k2].quote_amount = markets[k2].quote_amount + v._source.operation_history.op_object.pays.amount
-                    markets[k2].base_amount = markets[k2].base_amount + v._source.operation_history.op_object.receives.amount
-                    new = false
-
-                    break
-                end
-            end
-        end
-
-        if new then
-            markets[k] = {
-                quote = v._source.operation_history.op_object.pays.asset_id,
-                base = v._source.operation_history.op_object.receives.asset_id,
-                quote_amount = v._source.operation_history.op_object.pays.amount,
-                base_amount = v._source.operation_history.op_object.receives.amount
-            }
-        end
-    end
-end
-
-
 requests = require('requests')
 
 local queries = require('queries')
+local functions = require('functions')
 
 headers = {['Content-Type'] = 'application/json'}
 
@@ -65,7 +15,7 @@ total = json_body.hits.total
 scroll_id = json_body._scroll_id
 -- print(total)
 
-loop_fills(json_body.hits.hits)
+functions.loop_fills(json_body.hits.hits)
 
 while total > 0 do
 
@@ -80,7 +30,7 @@ while total > 0 do
 
     json_body, error = response.json()
 
-    loop_fills(json_body.hits.hits)
+    functions.loop_fills(json_body.hits.hits)
 
 end
 
---
+++ functions.lua
@@ -0,0 +1,55 @@
+local F = {}
+
+local function loop_fills(hits)
+
+    for k, v in pairs(hits) do
+
+        total = total - 1
+        --print("first: " .. total)
+
+        new = true;
+
+        if k > 1 then
+
+            for k2, v2 in pairs(markets) do
+                if v2.quote == v._source.operation_history.op_object.pays.asset_id and
+                        v2.base == v._source.operation_history.op_object.receives.asset_id then
+
+                    markets[k2].quote_amount = markets[k2].quote_amount + v._source.operation_history.op_object.pays.amount
+                    markets[k2].base_amount = markets[k2].base_amount + v._source.operation_history.op_object.receives.amount
+                    new = false
+
+                    break
+                end
+            end
+        end
+
+        if new then
+            markets[k] = {
+                quote = v._source.operation_history.op_object.pays.asset_id,
+                base = v._source.operation_history.op_object.receives.asset_id,
+                quote_amount = v._source.operation_history.op_object.pays.amount,
+                base_amount = v._source.operation_history.op_object.receives.amount
+            }
+        end
+    end
+
+end
+
+-- Utility function to print a recursive table keys and values
+local function DeepPrint (e)
+    -- if e is a table, we should iterate over its elements
+    if type(e) == "table" then
+        for k,v in pairs(e) do -- for every element in the table
+            print(k)
+            DeepPrint(v)       -- recursively repeat the same procedure
+        end
+    else -- if not, we can just print it
+        print(e)
+    end
+end
+
+F.loop_fills = loop_fills
+F.DeepPrint = DeepPrint
+
+return F
